dd33b84067cfcfeb666e4729aaf5cebd8bcd71cc
yanningH16 <632311638@qq.com>
2018-03-05 19:25:11 +0800

    打包

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {
       "/api": {
         // target: "http://182.61.29.51:8089", // 测试环境
-        target: "http://18.218.181.216:8089", // 线上
+        // target: "http://18.218.181.216:8089", // 线上
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
